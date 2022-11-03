ae55e99dc0293960e9d1de4c7e4650eb9d9c0216
Felipe Oliveira <felipeoliveira.santos89@gmail.com>
2022-11-03 17:23:59 -0300

    [00] - Refazendo projeto para Maven

--- primeirocrud/src/main/java/com/crud/primeirocrud/PrimeirocrudApplication.java
+++ primeirocrud/src/main/java/com/crud/primeirocrud/PrimeirocrudApplication.java
@@ -1,13 +1,13 @@
-package crud.client.crud;
+package com.crud.primeirocrud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CrudApplication {
+public class PrimeirocrudApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CrudApplication.class, args);
+		SpringApplication.run(PrimeirocrudApplication.class, args);
 	}
 
 }
--- primeirocrud/src/test/java/com/crud/primeirocrud/PrimeirocrudApplicationTests.java
+++ primeirocrud/src/test/java/com/crud/primeirocrud/PrimeirocrudApplicationTests.java
@@ -1,10 +1,10 @@
-package crud.client.crud;
+package com.crud.primeirocrud;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class CrudApplicationTests {
+class PrimeirocrudApplicationTests {
 
 	@Test
 	void contextLoads() {
